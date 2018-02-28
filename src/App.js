81f555177e828370f7a6d3a535ad5ead22ab6704
Ian Nastajus <ian.nastajus@gmail.com>
2018-02-27 22:38:55 -0500

    header & footer components added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
 import './App.css';
 
 /*
@@ -10,6 +13,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Footer />
       </div>
     );
   }
